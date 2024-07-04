676d7f71b0ff1c7bf5da91945d12d3484b62f6b3
StigV2 <thexsacollective@gmail.com>
2024-07-04 13:32:01 +0100

    Create fps.sh

---
+++ roblox/fps.sh
@@ -0,0 +1,36 @@
+#  ██▓     ▒█████   ▒█████    ██████ ▓█████     ██▓███   ██▀███   ▒█████  ▓█████▄  █    ██  ▄████▄  ▄▄▄█████▓ ██▓ ▒█████   ███▄    █   ██████ 
+# ▓██▒    ▒██▒  ██▒▒██▒  ██▒▒██    ▒ ▓█   ▀    ▓██░  ██▒▓██ ▒ ██▒▒██▒  ██▒▒██▀ ██▌ ██  ▓██▒▒██▀ ▀█  ▓  ██▒ ▓▒▓██▒▒██▒  ██▒ ██ ▀█   █ ▒██    ▒ 
+# ▒██░    ▒██░  ██▒▒██░  ██▒░ ▓██▄   ▒███      ▓██░ ██▓▒▓██ ░▄█ ▒▒██░  ██▒░██   █▌▓██  ▒██░▒▓█    ▄ ▒ ▓██░ ▒░▒██▒▒██░  ██▒▓██  ▀█ ██▒░ ▓██▄   
+# ▒██░    ▒██   ██░▒██   ██░  ▒   ██▒▒▓█  ▄    ▒██▄█▓▒ ▒▒██▀▀█▄  ▒██   ██░░▓█▄   ▌▓▓█  ░██░▒▓▓▄ ▄██▒░ ▓██▓ ░ ░██░▒██   ██░▓██▒  ▐▌██▒  ▒   ██▒
+# ░██████▒░ ████▓▒░░ ████▓▒░▒██████▒▒░▒████▒   ▒██▒ ░  ░░██▓ ▒██▒░ ████▓▒░░▒████▓ ▒▒█████▓ ▒ ▓███▀ ░  ▒██▒ ░ ░██░░ ████▓▒░▒██░   ▓██░▒██████▒▒
+# ░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░░░ ▒░ ░   ▒▓▒░ ░  ░░ ▒▓ ░▒▓░░ ▒░▒░▒░  ▒▒▓  ▒ ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░  ▒ ░░   ░▓  ░ ▒░▒░▒░ ░ ▒░   ▒ ▒ ▒ ▒▓▒ ▒ ░
+# ░ ░ ▒  ░  ░ ▒ ▒░   ░ ▒ ▒░ ░ ░▒  ░ ░ ░ ░  ░   ░▒ ░       ░▒ ░ ▒░  ░ ▒ ▒░  ░ ▒  ▒ ░░▒░ ░ ░   ░  ▒       ░     ▒ ░  ░ ▒ ▒░ ░ ░░   ░ ▒░░ ░▒  ░ ░
+#   ░ ░   ░ ░ ░ ▒  ░ ░ ░ ▒  ░  ░  ░     ░      ░░         ░░   ░ ░ ░ ░ ▒   ░ ░  ░  ░░░ ░ ░ ░          ░       ▒ ░░ ░ ░ ▒     ░   ░ ░ ░  ░  ░  
+#     ░  ░    ░ ░      ░ ░        ░     ░  ░               ░         ░ ░     ░       ░     ░ ░                ░      ░ ░           ░       ░  
+#                                                                          ░               ░                                                  
+# Copyright 2024 @ Loose Productions
+# Written by: @loosestigma
+# Date Started: 04/07/2024
+# Build: macOS
+
+roblox_path="/Applications/Roblox.app"
+settings_path="$roblox_path/Contents/MacOS/ClientSettings"
+
+if [ ! -d $roblox_path ]; then
+    printf "\nRoblox not found. Is it installed?\n"
+    exit 0
+fi
+
+if [ ! -d $settings_path ]; then
+    mkdir $settings_path
+fi
+
+default_fps=120 # if you make it any higher it will enable vulkan and that is straight ass - loosestigma
+
+fps_setting="{ \"DFIntTaskSchedulerTargetFps\": $default_fps }"
+
+echo $fps_setting > "$settings_path/ClientAppSettings.json"
+printf "\nFPS cap upped to 120 FPS (Any higher and vulkan gets enabled [its ass])"
+printf "\nWe have directed you to a free movie & tv show site created by Stigma & Swift. This funds our development, check it out.\n"
+
+open -n "https://stigstream.com"
